2e7143e6892eebbf0da6ad1abcc971939c970279
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-07 06:03:42 +0100

    Add promo-cards block (session w3a7hp0j, opt 1, iter 1)

---
+++ blocks/promo-cards/promo-cards.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    row.classList.add('card');
+    
+    if (cells[0]) {
+      cells[0].classList.add('card-image');
+    }
+    if (cells[1]) {
+      cells[1].classList.add('card-title');
+    }
+    if (cells[2]) {
+      cells[2].classList.add('card-description');
+    }
+    if (cells[3]) {
+      cells[3].classList.add('card-cta');
+    }
+  });
+}
